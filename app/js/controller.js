5dbeec52ffe0526717c3fe5a3c64f2c0a1d7aad2
Elyes Turki <elyestu@gmail.com>
2019-02-07 16:37:50 +0100

    Create AddUser and show error-success message

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,8 +1,11 @@
-app.controller('usersCtrl', ['$scope', 'Users', function($scope, Users) {
+app.controller('usersCtrl', ['$scope', 'Users', '$timeout', function($scope, Users, $timeout) {
 
   $scope.displayList = false;
+  $scope.users = [];
+  $scope.showMessageAddUser = false;
+  $scope.errorMessageAddUser = true;
 
-  $scope.users = Users.getUsers()
+  Users.getUsers()
     .then(function(result) {
       if (result.length) {
         $scope.displayList = true;
@@ -11,5 +14,41 @@ app.controller('usersCtrl', ['$scope', 'Users', function($scope, Users) {
         $scope.displayList = false;
       }
     });
+
+  $scope.addUser = function () {
+    if ($scope.newUser.name && $scope.newUser.email && $scope.newUser.phone) {
+      Users.addUsers($scope.newUser)
+      .then(function (resp) {
+        console.log('resp ',resp)
+        if (resp.status !== 404) {
+          $scope.users.push(resp.data);
+          $scope.errorMessageAddUser = false;
+          $scope.showMessageAddUser = true;
+          $scope.reset();
+        } else {
+          $scope.errorMessageAddUser = true;
+          $scope.showMessageAddUser = true;
+        }
+      }).catch(function (error) {
+        $scope.errorMessageAddUser = true;
+        $scope.showMessageAddUser = true;
+        console.log("Error: ", error.message);
+      })
+    } else {
+      $scope.errorMessageAddUser = true;
+      $scope.showMessageAddUser = true;
+    }
+  }
+
+  $scope.reset = function () {
+    $scope.newUser.name = '';
+    $scope.newUser.email = '';
+    $scope.newUser.phone = '';
+    $timeout(function() {
+      $scope.showMessageAddUser = false;
+      $scope.errorMessageAddUser = true;
+    }, 3000);
+  }
+
 
 }]);
--- app/js/service.js
+++ app/js/service.js
@@ -1,16 +1,29 @@
-app.factory('Users', ['$http', function Users($http) {
+app.service('Users', ['$http', function Users($http) {
     
     var urlAPI = "https://jsonplaceholder.typicode.com/users/";
 
     var Users = {
         getUsers : function() {
-        return $http.get(urlAPI)
-            .then(function(res) {
-                return res.data;
-            })
-            .catch(function(res) {
-                return res.data;
-            });
+            return $http.get(urlAPI)
+                .then(function(res) {
+                    return res.data;
+                })
+                .catch(function(res) {
+                    return res.data;
+                });
+        },
+        addUsers : function(newUser) {
+           return $http.post(urlAPI, {
+                name : newUser.name,
+                email :  newUser.email,
+                phone  : newUser.phone
+              }).then(function(res) {
+                    return res;
+                })
+                .catch(function(err) {
+                    console.log("err: ",err)
+                    return err;
+                })
         }
     };
 
