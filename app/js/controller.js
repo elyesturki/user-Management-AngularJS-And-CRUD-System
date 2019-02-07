559376038963f21a2548d410e5e721e5cb7c03da
Elyes Turki <elyestu@gmail.com>
2019-02-07 10:40:01 +0100

    Use ng-if directive (angularJS) to display list of Users only if was founded

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,9 +1,14 @@
 app.controller('usersCtrl', ['$scope', 'Users', function($scope, Users) {
 
+  $scope.displayList = false;
+
   $scope.users = Users.getUsers()
     .then(function(result) {
-      if (result) {
+      if (result.length) {
+        $scope.displayList = true;
         $scope.users = result
+      } else {
+        $scope.displayList = false;
       }
     });
 
